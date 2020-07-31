681972ed6549aff3e6d2e43e4989f2236b92e412
Anneloes <anneloes9405@hotmail.com>
2020-07-30 21:07:20 -0400

    basic idea of schedule in html/css, now making grid
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

---
+++ src/components/Schedule.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Schedule.css"
+
+export default function Schedule(props) {
+  return(
+    <div id="schedule">
+      <div class="schedule">
+
+        <h3 class="time-slot">8:00am</h3>
+        <div class="task monday">
+          <h4 class="task-title">Pickup</h4>
+          <span class="task-time">8:00am - 9:00am</span><br/>
+          <span class="task-day">Monday</span>
+        </div>
+
+        <h3 class="time-slot">9:00am</h3>
+        <div class="task task-5 monday">
+          <h4 class="task-title">Other</h4>
+          <span class="task-time">9:00am - 10:00am</span><br/>
+          <span class="task-day">Monday</span>
+        </div>
+      </div>
+
+    </div>
+  )
+}
