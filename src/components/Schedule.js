bd2154c28dea2f7f7e85967d864602cf02e97273
Anneloes <anneloes9405@hotmail.com>
2020-07-31 00:21:06 -0400

    basic grid/schedule idea done, now adding times in column and fixing css assignment to tasks
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -22,19 +22,20 @@ export default function Schedule(props) {
 
   return(
     <div id="schedule">
-      <div class="schedule">
+      <div class="grid">
+        <span class="day-slot" aria-hidden="true" style={{gridColumn: "sunday"}}>Sunday</span>
+        <span class="day-slot" aria-hidden="true" style={{gridColumn: "monday"}}>Monday</span>
+        <span class="day-slot" aria-hidden="true" style={{gridColumn: "tuesday"}}>Tuesday</span>
+        <span class="day-slot" aria-hidden="true" style={{gridColumn: "wednesday"}}>Wednesday</span>
+        <span class="day-slot" aria-hidden="true" style={{gridColumn: "thursday"}}>Thursday</span>
+        <span class="day-slot" aria-hidden="true" style={{gridColumn: "friday"}}>Friday</span>
+        <span class="day-slot" aria-hidden="true" style={{gridColumn: "saturday"}}>Saturday</span>
+
 
         <h3 class="time-slot">8:00am</h3>
-        <div class="task monday">
+        <div class="task monday task-1">
           <h4 class="task-title">Pickup</h4>
           <span class="task-time">8:00am - 9:00am</span><br/>
-          <span class="task-day">Monday</span>
-        </div>
-
-        <h3 class="time-slot">9:00am</h3>
-        <div class="task task-5 monday">
-          <h4 class="task-title">Other</h4>
-          <span class="task-time">9:00am - 10:00am</span><br/>
           <span class="task-day">Monday</span>
         </div>
       </div>
