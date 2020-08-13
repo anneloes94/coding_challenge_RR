21f6e95413374e876248511fdd1a69c3432227b4
Anneloes <anneloes9405@hotmail.com>
2020-08-13 18:04:52 -0400

    bug fixed: filter problem at Day component in Schedule.js
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- src/components/Day.js
+++ src/components/Day.js
@@ -3,17 +3,15 @@ import Task from "./Task"
 
 export default function Day(props) {
   const tasks = props.tasks.map((task, index) => {
-    if (props.day.tasks.includes(task.id)) {
-      return(
-        <Task
-          index={index}
-          dayName={props.dayName}
-          title={task.title}
-          startTime={task.start_time}
-          endTime={task.end_time}
-        />
-      )
-    }
+    return(
+      <Task
+        index={index}
+        dayName={props.dayName}
+        title={task.title}
+        startTime={task.start_time}
+        endTime={task.end_time}
+      />
+    )
   })
     
   return(
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -34,11 +34,12 @@ export default function Schedule(props) {
 
         {/* DAYS/TASKS */}
         {/* For every day in days state, assign a Day component: */}
+        {/* We filter tasks by the day they belong to */}
         {props.days.map(day => {
           return(
             day.name && 
             <Day
-              tasks={props.tasks}
+              tasks={props.tasks.filter(task => day.tasks.includes(task.id) )}
               day={day}
               dayName={day.name}
             />
