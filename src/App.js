75c63c40e863db08d122a9c2bdc5954a68e87605
Anneloes <anneloes9405@hotmail.com>
2020-08-15 16:48:04 -0400

    removed initial state declaration, initial tasksFilter function, setting up Redux within App
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -3,65 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import Schedule from "./components/Schedule"
 import { filterDaysByWeek, filterTasksByDriver } from "./helpers/selectors"
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
+
+console.log(store.getState())
 
 export default function App() {
-
-  // STATE
-  const [week, setWeek] = useState(1);
-  const [driver, setDriver] = useState({});
-  const [tasks, setTask] = useState([
-    {
-      id: 1,
-      title: "dropoff",
-      driver: 1,
-      start_time: "1000",
-      end_time: "1200"
-
-    },
-    {
-      id: 2,
-      title: "other",
-      driver: 3,
-      start_time: "1600",
-      end_time: "1800"
-    },
-    {
-      id: 3,
-      title: "pickup",
-      driver: 2,
-      start_time: "1500",
-      end_time: "1600"
-    }
-  ])
-  const [days, setDay] = useState([
-    {
-      id: 1,
-      name: "sunday",
-      tasks: [1]
-    },
-    {
-      id: 5,
-      name: "thursday",
-      tasks: [2]
-    },
-    {
-      id: 13,
-      name: "friday",
-      tasks: [3]
-    }
-  ])
-
-  // FUNCTIONS
-
-  const changeDriver = (newDriver) => {
-    setDriver(newDriver)
-  }
-
-  const changeWeek = (newWeek) => {
-    setWeek(newWeek)
-  }
-
-  // RENDERING
 
   return (
     <div className="App">
--- src/helpers/convertors.js
+++ src/helpers/convertors.js
@@ -28,4 +28,19 @@ function convertToClockTime(time) {
   return numberString + periodString;
 }
 
-export { convertToClockTime }
+function getDayRangeByWeek(week) {
+  // 1a. Get current week as a range of days
+  //    IN: week  - OUT: firstDay, lastDay
+  const firstDay = week * 7 - 6
+  const lastDay = week * 7
+
+  // 1b. Filter days for this range of days
+  //    IN: days, firstDay, lastDay   - OUT: days
+  let daysRange = [];
+  for (i = firstDay; i < lastDay; i++) {
+    daysRange.push(i)
+  }
+  return filteredDays
+}
+
+export { convertToClockTime, getDayRangeByWeek }
